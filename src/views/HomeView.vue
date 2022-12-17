9c16bf4bd2577366f07163aa84be9ee9f4e07dff
Bilal Ahmed <bilal91_@hotmail.com>
2022-12-17 15:06:27 +0000

    adds method to get random todos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,17 @@
 	<div class="about">
 		<v-app-bar color="deep-purple" dark>
 			<v-app-bar-title> Todo List </v-app-bar-title>
-			<v-spacer />
 		</v-app-bar>
+		<p>{{ $store.state.todos }}</p>
 	</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
-import ToDoItemCard from '../components/ToDoItemCrad.vue';
+// renames for correct import
+import ToDoItemCard from '../components/ToDoItemCard.vue';
 import store, { Todo } from '@/store';
+import Services from '../Services/index.js';
 
 @Component({})
 export default class HomeView extends Vue {
@@ -28,6 +30,28 @@ export default class HomeView extends Vue {
 		if (!this.isFormValid) {
 			return;
 		}
+	}
+	// adds method to get random todos with image
+	public async getToDos(): Promise<void> {
+		const Service = new Services();
+		let loaded = false;
+		console.log(loaded);
+		for (let i = 1; i < 5; i++) {
+			let todo = await Service.getTodo(i);
+			let image = await Service.getImage();
+			let newToDo = {
+				title: todo.title,
+				description: 'This ToDo is random',
+				id: new Date().getTime(),
+				isCompleted: todo.completed,
+				imageUrl: image.url,
+			};
+			store.dispatch('addToDo', newToDo);
+		}
+		loaded = true;
+	}
+	public mounted() {
+		this.getToDos();
 	}
 }
 </script>
