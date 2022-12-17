170cae5083f99b644a2cc956e0ea551ed961a480
Bilal Ahmed <bilal91_@hotmail.com>
2022-12-17 16:13:55 +0000

    changes to a ts file

--- src/Services/index.ts
+++ src/Services/index.ts
@@ -1,15 +1,15 @@
 export default class Services {
 	// the below methods get todos and images from external apis
 
-	async getTodos(number) {
-		let response = await fetch(
+	async getTodo(number: number): Promise<unknown> {
+		const response = await fetch(
 			`https://jsonplaceholder.typicode.com/todos/${number}`
 		).then((response) => response.json());
 		return response;
 	}
 	// the correct url has been added for a random image
-	async getImage() {
-		let response = await fetch('https://picsum.photos/200/300');
+	async getImage(): Promise<unknown> {
+		const response = await fetch('https://picsum.photos/200/300');
 		return response;
 	}
 }
