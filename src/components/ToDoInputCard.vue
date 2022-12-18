9f6ed57413793d864beda6eb1704485c05e0a92a
Bilal Ahmed <bilal91_@hotmail.com>
2022-12-18 08:49:23 +0000

    adds more form validation and creates the data variables for the v-models on the form

--- src/components/ToDoInputCard.vue
+++ src/components/ToDoInputCard.vue
@@ -14,7 +14,9 @@
 			required
 		></v-text-field>
 		<v-text-field v-model="ImageUrl" label="Image Url"></v-text-field>
-		<v-btn color="success" class="mr-4" @click="addToDo"> addToDo </v-btn>
+		<v-btn :disabled="!valid" color="success" class="mr-4" @click="addToDo">
+			addToDo
+		</v-btn>
 		<v-btn color="error" class="mr-4" @click="reset"> Reset </v-btn>
 	</v-form>
 </template>
@@ -31,16 +33,27 @@ import { ValidationProvider, ValidationObserver } from 'vee-validate';
 	},
 })
 export default class ToDoInputCard extends Vue {
+	// variables for the v-model which will be used to create a new todo
+	Title = '';
+	Description = '';
+	ImageUrl = '';
+	valid = false;
 	reset(): void {
 		(this.$refs.form as Vue & { reset: () => boolean }).reset();
+	}
+	addToDo(): void {
+		return;
 	}
 	titleRules = [
 		(v: string) => !!v || 'Title is required',
 		(v: string) => v.length <= 100 || 'Title must be less than 100 characters',
+		(v: string) => v.length >= 10 || 'Title must be more than 10 characters',
 	];
 	descriptionRules = [
 		(v: string) => !!v || 'Description is required',
-		(v: string) => v.length <= 1000 || 'Title must be less than 100 characters',
+		(v: string) =>
+			v.length <= 1000 || 'Title must be less than 1000 characters',
+		(v: string) => v.length >= 10 || 'Title must be more than 10 characters',
 	];
 }
 </script>
